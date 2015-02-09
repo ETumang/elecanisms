62288fa68bca55c987bac43bc87919ea839208f6
ETumang <emily.tumang@gmail.com>
2015-02-09 08:26:57 -0500

    Fixing flip code

--- project_1/main.c
+++ project_1/main.c
@@ -7,24 +7,42 @@
 #include <stdio.h>
 
 #define  FULL_DUTY_CYCLE 2^16
-#define  MAX_VOLTAGE 2^10 << 5
-#define  FULL_ANGLE MAX_VOLTAGE*3
+#define  MAX_VOLTAGE 2^12
+
+#define  DATA_PERIOD .1
+#define  SEND_SCALAR  1
+
+int get_pos = 1;
+int send_data = 0;
+int flipped = 0;
+
+
+void data_timing(_TIMER *timer){
+	send_data ++;
+	get_pos = 1;
+}
 
 void main(void){
+
 	init_pin();
 	init_oc();
 	init_ui();
 	init_timer();
 	init_uart();
 
-	int voltage;
-	int angle = 0;
-	int diff;
-	int prev_voltage = 0;
-
+	uint16_t rawPos;
+	long int updatedPos = 0;
+	long int rawDiff;
+	long int lastRawPos=0;
+	long int lastLastRawPos;
+	long int lastRawDiff;
+	long int rawOffset;
+	long int lastRawOffset;
+	int flips;
+	long int offset;
 	int i;
 
-	timer_setPeriod(&timer1, .1);
+	timer_setPeriod(&timer1, .001);
 	timer_start(&timer1);
 
 	for(i = 1; i < 9; i++){
@@ -44,31 +62,57 @@ void main(void){
 	pin_clear(&D[8]);//not inverting input
 
 	pin_analogIn(&A[5]);
+	pin_analogIn(&A[0]);
+
+	//timer_setPeriod(&timer1, .01);
+	timer_enableInterrupt(&timer1);
+	timer_every(&timer1, DATA_PERIOD, data_timing);
 
 	led_on(&led1);
 
+	rawPos = pin_read(&A[0])>>6;
+	lastRawPos = pin_read(&A[0])>>6;
+	lastLastRawPos = pin_read(&A[0])>>6;
+
 	while(1){
-
-		voltage = pin_read(&A[5]);
-
-		diff = voltage - prev_voltage;
-
-		if (abs(diff) < MAX_VOLTAGE){
-			angle += diff;
+		if(get_pos){
+			rawPos = pin_read(&A[0])>>6;
+		
+  // Calculate differences between subsequent MR sensor readings
+  rawDiff = rawPos - lastRawPos;          //difference btwn current raw position and last raw position
+  lastRawDiff = rawPos - lastLastRawPos;  //difference btwn current raw position and last last raw position
+  rawOffset = abs(rawDiff);
+  lastRawOffset = abs(lastRawDiff);
+  
+  // Update position record-keeping vairables
+  lastLastRawPos = lastRawPos;
+  lastRawPos = rawPos;
+  
+  // Keep track of flips over 180 degrees
+  if((lastRawOffset > 800) && (!flipped)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
+    if(lastRawDiff > 0) {        // check to see which direction the drive wheel was turning
+      flips--;              // cw rotation 
+    } else {                     // if(rawDiff < 0)
+      flips++;              // ccw rotation
+    }
+  // check to see if the data was good and the most current offset is above the threshold
+    updatedPos = rawPos + flips*rawOffset; // update the pos value to account for flips over 180deg using the most current offset 
+    offset = rawOffset;
+    
+    flipped = 1;        // set boolean so that the next time through the loop won't trigger a flip
+  } else {                        // anytime no flip has occurred
+    updatedPos = rawPos + flips*rawOffset; // need to update pos based on what most recent offset is 
+    flipped = 0;
+  }
+}
+		
+		if(send_data == SEND_SCALAR){
+			
+			printf("%li\n",(long int)(rawPos));
+			send_data = 0;
+			led_toggle(&led3);
 		}
-		else{
-			if(diff <0){
-				angle += diff + MAX_VOLTAGE;
-			}
-
-			else{
-				angle += diff - MAX_VOLTAGE;
-			}
-		}
-		led_on(&led2);
-		printf("Difference : %d\n", diff);
-		printf("Current Angle: %d\n",angle);
-		led_toggle(&led3);
 		/*Don't forget to check your serial port and baud rate*/
 	}
 }
+
