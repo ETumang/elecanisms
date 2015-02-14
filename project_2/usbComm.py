af013a6b2d2b6354e77311afdc2eb9c1f6664043
ETumang <emily.tumang@gmail.com>
2015-02-14 11:30:00 -0500

    Added rudimentary GUI

--- project_2/usbComm.py
+++ project_2/usbComm.py
@@ -2,41 +2,44 @@ import usb.core
 
 class usbStates:
 
-	def __init__(self):
-		self.SPRING = 0
-		self.DAMPER = 1
-		self.TEXTURE = 2
-		self.WALL = 3
+    def __init__(self):
+        self.SPRING = 0
+        self.DAMPER = 1
+        self.TEXTURE = 2
+        self.WALL = 3
 
-		  self.dev = usb.core.find(idVendor = 0x6666, idProduct = 0x0003)
+        self.dev = usb.core.find(idVendor = 0x6666, idProduct = 0x0003)
         if self.dev is None:
             raise ValueError('no USB device found matching idVendor = 0x6666 and idProduct = 0x0003')
         self.dev.set_configuration()
 
     def close(self):
-    	self.dev = None
-
+        self.dev = None
     def spring(self):
-    	try:
+        try:
             self.dev.ctrl_transfer(0x40, self.SPRING)
+            print("spring")
         except usb.core.USBError:
             print "Could not set board to SPRING."
 
     def damper(self):
-    	try:
+        try:
             self.dev.ctrl_transfer(0x40, self.DAMPER)
+            print("damper")
         except usb.core.USBError:
             print "Could not set board to DAMPER."
 
     def texture(self):
-    	try:
+        try:
             self.dev.ctrl_transfer(0x40, self.TEXTURE)
+            print("texture")
         except usb.core.USBError:
             print "Could not set board to TEXTURE."
 
-	def texture(self):
-    	try:
+    def wall(self):
+        try:
             self.dev.ctrl_transfer(0x40, self.WALL)
+            print("wall")
         except usb.core.USBError:
             print "Could not set board to WALL."
 
